c095b196524d1889b498bc493ccb526672085dd8
Timothy Odebunmi <dev.brymes@gmail.com>
2024-04-25 07:53:20 +0100

    Update api.ts

--- src/features/chat/lib/api.ts
+++ src/features/chat/lib/api.ts
@@ -3,8 +3,8 @@ import { ChatAPI } from './types';
 
 // export const baseURL = "https://naerospace-chat-staging-f79d5af35fb9.herokuapp.com";
 // export const wsURL = "wss://naerospace-chat-staging-f79d5af35fb9.herokuapp.com/ws";
-export const baseURL = "http://158.220.111.179:8004";
-export const wsURL = "wss://158.220.111.179:8004";
+export const baseURL = "https://admin.sabrecwa.com/brymes";
+export const wsURL = "wss://admin.sabrecwa.com/brymes/ws";
 
 const axios = Axios.create({
   baseURL
