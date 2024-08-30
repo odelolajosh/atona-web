66c141778885ff842047369d5cfeb145550f8ffe
Joshua Odelola <oladapoodelola@gmail.com>
2024-08-30 07:59:17 +0100

    refactor: update import statement for common routes

--- src/features/common/routes.tsx
+++ src/features/common/routes.tsx
@@ -1,5 +1,5 @@
 import { RouteObject } from "react-router-dom";
-import { Stray } from "./away";
+import { Stray } from "./stray";
 
 const commonRoutes: RouteObject[] = [
   { path: '*', element: <Stray /> }
